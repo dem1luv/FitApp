a1606f5300eb7a8c1cb872a77455124b21fa14a0
LyaShket <dingofleep@gmail.com>
2020-03-15 09:30:27 +0200

    a work for portfolio is done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// burger
+$(window).on("load resize", function(){
+	if ($(window).width() < 768){
+		if (document.querySelector(".menu__button.active"))
+			$('.menu__box,.menu__button').addClass('active');
+			$('body').add('lock');
+	} else {
+		if (document.querySelector(".menu__button.active"))
+			$('.menu__box,.menu__button').removeClass('active');
+			$('body').removeClass('lock');
+	}
+});
+$(document).ready(function(){
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.menu__box').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
 //parallax
 $(document).ready(function(){
     new WOW().init();
@@ -9,17 +27,22 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".header__nav,.header__logo,.menu__logo,.footer__links,.home__button-container,.places__button-container").on("click","a", function (event) {
         event.preventDefault();
-        let id  = $(this).attr('href'),
+		let id  = $(this).attr('href');
+		if (id != "#") {
 			top = $(id).offset().top;
-		top -= screen.width < 768 ? 57 : 0;
-		$('body,html').animate({scrollTop: top}, 1000);
+			top -= screen.width < 768 ? 57 : 0;
+			$('body,html').animate({scrollTop: top}, 1000);
+		}
 	});
 	$(".menu__box").on("click","a", function (event) {
 		event.preventDefault();
-		let id  = $(this).attr('href'),
-			top = $(id).offset().top;
-		$('.menu__box,.header__burger').removeClass('active');
-		$('body').removeClass('lock');
-		$('body,html').animate({scrollTop: top - 57}, 1000);
+		let id  = $(this).attr('href');
+		if (id != "#") {
+			let top = $(id).offset().top;
+			top -= screen.width < 768 ? 58 : 0;
+			$('.menu__box,.header__burger').removeClass('active');
+			$('body').removeClass('lock');
+			$('body,html').animate({scrollTop: top - 57}, 1000);
+		}
 	})
 });
